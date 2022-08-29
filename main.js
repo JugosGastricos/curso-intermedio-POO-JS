56fcd76e5e1ec66c574fb7af30b66e63dc349748
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2022-08-29 13:20:48 -0500

    Clase 9: aplicando recursividad para hacer deep copy

--- main.js
+++ main.js
@@ -132,3 +132,44 @@ function recursiva (parametro) {
 }
 
 recursiva(numeritos)
+
+//Clase 9
+
+function isObject(subject){
+    return typeof(subject) == 'Object';
+}
+
+function isArray(subject){
+    return typeof(subject) == 'Array';
+}
+
+function deepCopy(subject){
+    let copySubject;
+
+    const subjectIsArray = isArray(subject);
+    const subjectIsObject = isObject(subject);
+
+    if (subjectIsArray) {
+        copySubject = [];
+    } else if (subjectIsObject) {
+        copySubject = {};
+    } else {
+        return subject;
+    }
+
+    for (key in subject){
+        const keyIsObject = isObject(subject[key]);
+
+        if (keyIsObject) {
+            copySubject[key] = deepCopy(subject[key]);
+        } else {
+            if (subjectIsArray) {
+                copySubject.push(subject[key]);
+            } else {
+                copySubject[key] = subject[key];
+            }
+        }
+    }
+    
+    return copySubject;
+}
